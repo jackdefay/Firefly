1269f3c4445931c99432d3b508c826e5d4cef3e5
Jack Defay <3jackattack3@gmail.com>
2017-01-22 18:08:15 -0500

    fixed double counting issue

--- FireflyV7/FireflyV7.ino
+++ FireflyV7/FireflyV7.ino
@@ -229,7 +229,7 @@ void updateAvg(){
 	}
 
 	if(numberOn > 0){
-		avgOffset = (double) tempSum/(numberOn+1);  //calculates the average, based on the sum variable and the number of fireflies variable
+		avgOffset = (double) tempSum/(numberOn);  //calculates the average, based on the sum variable and the number of fireflies variable
 		restrictNoise(tempSum, numberOn, distSelf, dist1, dist2, dist3);  //although avgOffset is not passed into this function, avgOffset is used in and recalculated in this function
 	}
 }
